8031c1e8d0108f314e4c401a7c57f4766a3af45a
Joseph Lukasik <jglukasik@wisc.edu>
2015-02-06 20:19:34 -0600

    Name files sequentially, not on times. Documented parameters

--- run_ode.m
+++ run_ode.m
@@ -1,24 +1,29 @@
+% Do we want to enforce P to be nonnegative? What if we have more medicine than sick people?
 options = odeset('RelTol', 1e-4, 'NonNegative', [1 2 3 4]);
 
-[t,pop] = ode45(@(t,pop) spar(t,pop,params), [0 100], pop0, options);
+[t,pop] = ode45(@(t,pop) spar(t,pop,params), [0 102], pop0, options);
 
-P1 = plot(t,pop);
-legend('S','P','A','R');
+P1 = plot(t,pop,t,sum(pop,2));
+legend('S','P','A','R','total');
 
-time = datestr(now, 31);
+% Find the number of the last file
+files = dir('figures');
+m = regexp(files(end).name, ['\d+'], 'match'); 
+file_num = sprintf('%04d', str2num(m{1}) + 1);
 
-plot_name = strcat('figures/', time, '.pdf');
+
+plot_name = strcat('figures/fig', file_num, '.pdf');
 saveas(gcf, plot_name);
 
-file_name = strcat('figures/', time, '.txt');
+file_name = strcat('figures/fig', file_num, '.txt');
 fileID = fopen(file_name, 'w');
 fprintf(fileID, 'Initial S, P, A, R values\n');
-fprintf(fileID, '%f\n', pop0);
+fprintf(fileID, '%16f\n', pop0);
 fprintf(fileID, '\nParameters\n');
-fprintf(fileID, 'beta\t%f\n',   params.beta);
-fprintf(fileID, 'mu\t%f\n',     params.mu);
-fprintf(fileID, 'gamma\t%f\n',  params.gamma);
-fprintf(fileID, 'alpha\t%f\n',  params.alpha);
-fprintf(fileID, 'b\t%f\n',      params.b);
-fprintf(fileID, 'd\t%f\n',      params.d);
+fprintf(fileID, 'beta\t%16f\n',   params.beta);
+fprintf(fileID, 'mu\t%16f\n',     params.mu);
+fprintf(fileID, 'gamma\t%16f\n',  params.gamma);
+fprintf(fileID, 'alpha\t%16f\n',  params.alpha);
+fprintf(fileID, 'b\t%16f\n',      params.b);
+fprintf(fileID, 'd\t%16f\n',      params.d);
 fclose(fileID);
